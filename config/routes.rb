8d6ac117c6f57f52e99edce2c61478c52e8a45b6
Alex Ovsiychuk <b13u13@gmail.com>
2016-01-15 22:05:07 +0200

    Add Sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
  	root          'static_pages#home'
 
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'signin'  => 'sessions#new'
+  delete 'signout' => 'sessions#destroy'
 
 end
